3568d503dabaa6adbf6935e291c00ab787175b98
omprojesi <71323387+omprojesi@users.noreply.github.com>
2020-09-17 20:24:44 +0300

    Airdropper kontratının Ownable versiyonu

---
+++ AirdropperSafe.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.6.0;
+
+interface IToken {
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function transfer(address to, uint256 value) external returns (bool);
+}
+
+contract AirdropperSafe {
+    
+    address private _owner;
+    
+    constructor () public {
+        _owner = msg.sender;
+    }
+
+    function airdrop(address mainContract, address[] calldata addresses, uint256[] calldata amounts, uint256 totalAmount) external returns (bool) {
+        
+        require(_owner == msg.sender, "Yonetici degilsiniz");
+        IToken myToken = IToken(mainContract);
+        myToken.transferFrom(msg.sender, address(this), totalAmount);
+        
+        uint256 length = addresses.length;
+        for (uint256 i = 0; i < length; i++) {
+             myToken.transfer(addresses[i], amounts[i]);
+        }
+        return true;
+    }
+    
+    function airdropDeflationary(address mainContract, address[] calldata addresses, uint256[] calldata amounts) external returns (bool) {
+        require(_owner == msg.sender, "Yonetici degilsiniz");
+        IToken myToken = IToken(mainContract);
+        
+        uint256 length = addresses.length;
+        for (uint256 i=0; i < length; i++) {
+            myToken.transferFrom(msg.sender, addresses[i], amounts[i]);
+        }
+        return true;
+        
+    }
+}
